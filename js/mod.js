46bfbc3bbb7838587a30c23beab7c30ac87a0dd8
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2023-01-12 19:40:25 +0300

    0.3.6.1

--- js/mod.js
+++ js/mod.js
@@ -13,13 +13,14 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.3.6",
+	num: "0.3.6.1",
 	name: "Literally nothing",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<br><h3 style='color: #00CED1;'>v0.3.6 - Artifacts</h3><br>
-			<span style='color: #808080'>- Just added a preview of some mechanic (not a content update)</span>
+	<br><h3 style='color: #00CED1;'>v0.3.6.1 - Artifacts</h3><br>
+			<span style='color: #808080'>- Just added a preview of some mechanic (not a content update)<br>
+			- Added <i>Current endgame</i> text</span>
 	<br><h3 style='color: #00CED1;'>v0.3.5.1</h3><br>
 			<span style='color: #808080'>- Decrease costs of some essence upgrades and Artifacts cost</span>
 	<br><h3 style='color: #00CED1;'>v0.3.4</h3><br>
@@ -81,11 +82,13 @@ function addedPlayerData() { return {
 
 // Display extra things at the top of the page
 var displayThings = [
+function(){return "Current endgame: 2 artifact slots"},
 ]
+
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("e280000000"))
+	return player.a.points.gte(2))
 }
 
 
